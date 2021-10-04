056652cd07a6dcff386e82db83323df477e7638c
heb1k0 <alejandrozuriguel@gmail.com>
2021-10-04 11:53:01 +0200

    nivell 3

--- nivell3/ex1j.js
+++ nivell3/ex1j.js
@@ -1,18 +1,20 @@
-function mostrar(number){
-    console.log(number);
+function mostrar(){
+    for (var i = 0; i < 10; i++) {
+        console.log(i)
+    }
 }
 
 var arrFunc = [
-    mostrar(0),
-    mostrar(1),
-    mostrar(2),
-    mostrar(3),
-    mostrar(4),
-    mostrar(5),
-    mostrar(6),
-    mostrar(7),
-    mostrar(8),
-    mostrar(9),
+    mostrar(),
+    mostrar(),
+    mostrar(),
+    mostrar(),
+    mostrar(),
+    mostrar(),
+    mostrar(),
+    mostrar(),
+    mostrar(),
+    mostrar(),
 ]
 
 for (var i = 0; i < arrFunc.length; i++) {
