4617ec9a1e0124109ac65564c9b7f090cb826270
heb1k0 <alejandrozuriguel@gmail.com>
2021-10-04 11:44:53 +0200

    exercicis

---
+++ nivell2/ex2.js
@@ -0,0 +1,5 @@
+function mostrarUsuario(usuario, apellido){
+    console.log(`Hola ${usuario} ${apellido}`)
+}
+
+mostrarUsuario("Alejandro","Zuriguel")
---
+++ nivell3/ex1j.js
@@ -0,0 +1,20 @@
+function mostrar(number){
+    console.log(number);
+}
+
+var arrFunc = [
+    mostrar(0),
+    mostrar(1),
+    mostrar(2),
+    mostrar(3),
+    mostrar(4),
+    mostrar(5),
+    mostrar(6),
+    mostrar(7),
+    mostrar(8),
+    mostrar(9),
+]
+
+for (var i = 0; i < arrFunc.length; i++) {
+    arrFunc[i];
+}
